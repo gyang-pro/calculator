78c23f0b94f4f3edf776e8f484e302650ffca699
gyang-pro <6613460+gyang-pro@users.noreply.github.com>
2023-09-06 16:26:18 -0700

    Add operate function that does basic math computation such as adding, subtracting, multiplying, and dividing

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function operate(num1, operator, num2) {
+    let result;
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+    }
+    return result;
+}
